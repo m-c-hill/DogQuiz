de84a4ac442f4514742c2912310dd28d2ce8a197
m-c-hill <matthewhill2595@gmail.com>
2021-08-20 11:16:50 +0100

    Update to the game logic

--- app.js
+++ app.js
@@ -7,18 +7,26 @@ let breedArray = [];
 
 generateDog();
 
+/**
+ * Function to reset the game
+ */
 function resetGame() {
     userScore = 0;
     livesLeft = 3;
     location.reload();
 }
 
-function testClick(value) {
+/**
+ * User selects a dog, updates game according to if user was correct or not
+ * @param {string} value player's choice of breed 
+ */
+function chooseDog(value) {
     if (value === breed) {
         result("win");
         console.log("Correct!");
         updateScore();
-        generateDog();
+        toggleNext("on");
+        toggleClick("off");
     } else {
         result("lose");
         console.log("Incorrect!");
@@ -27,42 +35,48 @@ function testClick(value) {
     checkEndGame();
 }
 
-async function run() {
-    await generateDogBreeds();
-    await generateDog();
-}
-
+/**
+ * Increment the user score by 1 when the player chooses a dog breed correctly
+ */
 function updateScore() {
     userScore += 1;
     const score = document.querySelector('#scoreNum');
     score.innerText = userScore;
 }
 
+/**
+ * Lose a life if the player chooses the incorrect breed
+ */
 function loseLife() {
     livesLeft -= 1;
     const lives = document.querySelector('#lives');
-    console.log(livesLeft);
-    lives.innerText = '🐶'.repeat(livesLeft);
+    lives.textContent = '🐶'.repeat(livesLeft);
 }
 
+/**
+ * Check if the game has been won (five points scored) or lost (all lives gone)
+ */
 function checkEndGame() {
     if (userScore >= 5) {
         alert("You Win!");
+        resetGame();
     } else if (livesLeft <= 0) {
         alert("Game over");
         resetGame();
     }
 }
 
-function gameover() {
-    console.log("GAME OVER!");
-}
-
+/**
+ * Function to update the image of the new dog
+ */
 function updateImage() {
     const dog = document.querySelector('#dog')
     dog.src = dogURL;
 }
 
+/**
+ * Function to update the buttons with the new selection of dog breeds
+ */
 function updateButtons() {
     buttons = document.querySelectorAll(".breed");
     let i = 0;
@@ -73,11 +87,9 @@ function updateButtons() {
     }
 }
 
-// Add event listeners for all four buttons. When button is clicked, the data (breed name) is passed into checkGuess
-function checkGuess(guess) {
-    // Checks if guess is in breedArray
-}
-
+/**
+ * Fetch list of all dog breeds using dog API and append to array
+ */
 async function generateDogBreeds() {
     try {
         const response = await fetch("https://dog.ceo/api/breeds/list/all")
@@ -90,6 +102,9 @@ async function generateDogBreeds() {
     }
 }
 
+/**
+ * Generate a random dog breed and photo using the dog API
+ */
 async function generateDog() {
     try {
         await generateDogBreeds();
@@ -105,12 +120,9 @@ async function generateDog() {
     }
 }
 
-async function interim() {
-    await generateDogBreeds();
-    generateRandomBreeds();
-    updateButtons();
-}
-
+/**
+ * Generate a random array of four dogs (including correct dog) to be used as options in the game
+ */
 function generateRandomBreeds() {
     breedArray = [breed];
     let j = 0;
@@ -122,15 +134,24 @@ function generateRandomBreeds() {
         }
     }
     breedArray = shuffle(breedArray);
-    console.log(breedArray);
 }
 
+/**
+ * Function to retrieve the breed of the dog from dog url returned by the API
+ * @param {string} url Dog url 
+ * @returns Dog breed
+ */
 function getBreed(url) {
     const regex = "(?<=breeds\/)(.*)(?=\/)";
     return url.match(regex)[0].split('-')[0]
 }
 
+/**
+ * Function to display the result of a player's choice
+ * @param {string} res Result 
+ */
 function result(res) {
+    toggleResult("on");
     const elem = document.querySelector("#result");
     switch (res) {
         case "win":
@@ -139,11 +160,86 @@ function result(res) {
             break;
         case "lose":
             elem.innerText = "Not quite, try again"
-            elem.setAttribute("style", "background-color: darkred;");
+            elem.setAttribute("style", "background-color: darkred");
             break;
     }
 }
 
+/**
+ * Function to toggle displaying the next button on or off
+ */
+function toggleNext(option) {
+    const button = document.querySelector("#next");
+    switch (option) {
+        case "on":
+            button.setAttribute("style", "display: inline-block");
+            break;
+        case "off":
+            button.setAttribute("style", "display: none");
+            break;
+    }
+}
+
+/**
+ * Function to toggle displaying the result banner on or off
+ */
+function toggleResult(option) {
+    const result = document.querySelector("#result");
+    switch (option) {
+        case "on":
+            result.setAttribute("style", "display: inline-block");
+            break;
+        case "off":
+            result.setAttribute("style", "display: none");
+            break;
+    }
+}
+
+/**
+ * Function to toggle if a button is clickable.
+ * Used after a player has completed their turn but not selected "Next Dog" yet.
+ */
+function toggleClick(option) {
+    const buttons = document.querySelectorAll(".breed");
+    switch (option) {
+        case "on":
+            for (let button of buttons) {
+                button.disabled = false;
+            }
+            break;
+        case "off":
+            console.log("click toggle")
+            for (let button of buttons) {
+                button.disabled = true;
+            }
+            break;
+    }
+}
+
+/**
+ * Tasks to execute when user selects the "Next Dog" button
+ */
+function next() {
+    generateDog();
+    toggleNext("off");
+    toggleResult("off");
+    toggleClick("on");
+
+}
+
+/**
+ * Method to capitalise the first character of a string
+ * @returns Capitalised string
+ */
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+/**
+ * Method to shuffle the positions of elements in an array
+ * @param {Array} a array to shuffle 
+ * @returns Array with randomised positions
+ */
 function shuffle(a) {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
@@ -153,8 +249,4 @@ function shuffle(a) {
         a[j] = x;
     }
     return a;
-}
-
-String.prototype.capitalize = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1);
 }
